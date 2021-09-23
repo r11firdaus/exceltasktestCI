24c717930933875eb3c44837691e51b993878bb5
Reza Firdaus <azerus98@gmail.com>
2021-09-23 10:49:35 +0700

    add cache for comment and add index for username in user table

---
+++ db/migrate/20210923034118_add_index_to_username_column_on_user.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsernameColumnOnUser < ActiveRecord::Migration[6.1]
+  def change
+    add_index :users, :username
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_034807) do
+ActiveRecord::Schema.define(version: 2021_09_23_034118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_09_10_034807) do
     t.string "province"
     t.string "city"
     t.string "district"
+    t.index ["username"], name: "index_users_on_username"
   end
 
   add_foreign_key "comments", "posts"
