3843605fa7918d48efebc7d4fe73cefc2c68ba93
Reza Firdaus <azerus98@gmail.com>
2021-09-23 09:32:19 +0700

    fix rubocop in session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
+# settings for session
 class Session < ApplicationRecord
-	def self.sweep(time = 1.hour)
-		if time.is_a?(String)
-			time = time.split.inject { |count, unit| count.to_i.send(unit) }
-		end
-	where("updated_at < ? OR created_at < ?", time.ago.to_s(:db), 2.days.ago.to_s(:db)).delete_all
-	end
+  def self.sweep(time = 1.hour)
+    time = time.split.inject { |count, unit| count.to_i.send(unit) } if time.is_a?(String)
+    where('updated_at < ? OR created_at < ?', time.ago.to_s(:db), 2.days.ago.to_s(:db)).delete_all
+  end
 end
