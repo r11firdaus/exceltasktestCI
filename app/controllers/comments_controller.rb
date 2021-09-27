2c73130d0529937b311f254fb4ca73457766f103
Reza Firdaus <azerus98@gmail.com>
2021-09-27 13:13:58 +0700

    add remove comment on first load or when new comment added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,23 +9,12 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(comment_params)
     @comment.commenter = session[:username]
     if @comment.save
-      # render js: js_to_create_comment(@comment)
+      respond_to do |format|
+        format.js
+      end
       # ActionCable.server.broadcast "comment_channel", {content: @comment, sender: session[:username]}
     end
   end
-
-  # def js_to_create_comment(comment)
-  #   "$('#comment').append(`
-  #       <div class='card bg-light my-2'>
-  #         <div class='card-body'>
-  #           <strong class='card-title'>#{comment.commenter}</strong>
-  #           <p>#{comment.body}</p>
-  #           <a href='/posts/#{comment.post_id}/comments/#{comment.id}' data-confirm='Are you sure' data-method='delete'>Delete Comment</a>
-  #         </div>
-  #       </div>
-  #    `)
-  #    $('#comment_body').val('')"
-  # end
 
   def destroy
     @post = Post.find(params[:post_id])
