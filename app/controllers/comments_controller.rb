525825210e741f5540e6468de9e75ec38bd09149
Reza Firdaus <azerus98@gmail.com>
2021-09-27 09:55:56 +0700

    set username from commenter from controllwe(not in front end )

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,10 +9,23 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(comment_params)
     @comment.commenter = session[:username]
     if @comment.save
-    #  # ActionCable.server.broadcast "comment_channel", {content: @comment, sender: session[:username]}
-    # redirect_to(post_path(@post))
+      # render js: js_to_create_comment(@comment)
+      # ActionCable.server.broadcast "comment_channel", {content: @comment, sender: session[:username]}
     end
   end
+
+  # def js_to_create_comment(comment)
+  #   "$('#comment').append(`
+  #       <div class='card bg-light my-2'>
+  #         <div class='card-body'>
+  #           <strong class='card-title'>#{comment.commenter}</strong>
+  #           <p>#{comment.body}</p>
+  #           <a href='/posts/#{comment.post_id}/comments/#{comment.id}' data-confirm='Are you sure' data-method='delete'>Delete Comment</a>
+  #         </div>
+  #       </div>
+  #    `)
+  #    $('#comment_body').val('')"
+  # end
 
   def destroy
     @post = Post.find(params[:post_id])
