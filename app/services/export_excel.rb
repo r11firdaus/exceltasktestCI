b33dc07f1fc83335cc10a437b9b72b0e41f09085
Reza Firdaus <azerus98@gmail.com>
2021-09-27 16:39:22 +0700

    code quality in service by rubocop suggest

--- app/services/export_excel.rb
+++ app/services/export_excel.rb
@@ -1,25 +1,28 @@
+# frozen_string_literal: true
+
+# processing excel file for user request
 class ExportExcel
-	def initialize(page:, role:, type:, id:)
-		@role = role
-		@type = type
-		@id = id
-		@page = page
-	end
-	
-	def export
-		@role == 'admin' ? excel_admin : excel_writer
-	end
+  def initialize(page:, role:, type:, id:)
+    @role = role
+    @type = type
+    @id = id
+    @page = page
+  end
 
-	def excel_admin
-		@type == 'all' ? Post.all : Post.order('created_at DESC').limit(10).offset(@page * 10 || 1)
-	end
+  def export
+    @role == 'admin' ? excel_admin : excel_writer
+  end
 
-	def excel_writer
-   		if @type == 'all'
-            Post.all.where('user_id = ?', @id)
-        else
-            Post.order('created_at DESC').limit(10)
-                .offset(page * 10 || 1).where('user_id = ?', @id)
-        end
-  	end
+  def excel_admin
+    @type == 'all' ? Post.all : Post.order('created_at DESC').limit(10).offset(@page * 10 || 1)
+  end
+
+  def excel_writer
+    if @type == 'all'
+      Post.all.where('user_id = ?', @id)
+    else
+      Post.order('created_at DESC').limit(10)
+          .offset(page * 10 || 1).where('user_id = ?', @id)
+    end
+  end
 end
--- app/services/export_pdf.rb
+++ app/services/export_pdf.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
+# processing pdf from post data
 class ExportPdf
-	def initialize(post:)
-		@post = post
-	end
-	
-	def export
-		save_path = Rails.root.join('public', "#{@post.id}.pdf")
-	    if File.exist?(save_path)
-	      # send pdf data
-	      return save_path
-	    else
-	      # kick off the job and render the default template for this action
-	      AddPdfJob.perform_now(@post)
-	      return save_path
-	    end
-	end
+  def initialize(post:)
+    @post = post
+  end
+
+  def export
+    save_path = Rails.root.join('public', "#{@post.id}.pdf")
+    if File.exist?(save_path)
+    # send pdf data
+    else
+      # kick off the job and render the default template for this action
+      AddPdfJob.perform_now(@post)
+    end
+    save_path
+  end
 end
