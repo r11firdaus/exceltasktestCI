6c1f4a61a7ac372a9b99643428df532b27d09460
Reza Firdaus <azerus98@gmail.com>
2021-09-30 09:39:45 +0700

    add bootstrap JS and toogle in navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 //= require popper
-//= require bootstrap
+import "bootstrap"
 require("jquery")
 
 
