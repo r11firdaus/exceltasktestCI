9d59793cb022542a86e020fbfbd120e37b7635aa
Reza Firdaus <azerus98@gmail.com>
2021-10-18 14:03:48 +0700

    add user for test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+	let(:user) {
+    User.new(username: 'testuser',
+						 password: '1234',
+						 province: 'JAWA BARAT',
+						 city: 'CIMAHI',
+						 district: 'CIMAHI UTARA')
+  }
+
 	describe "Associations" do
   	#it { should belong_to(:user).without_validating_presence }
 		it { should belong_to(:user) }
